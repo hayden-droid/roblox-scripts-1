ff983352022fec9177949d1e7c03686404cc19ce
strawbewwy <36744788+yabujin@users.noreply.github.com>
2021-07-30 23:45:16 +0300

    Create kayeffcee.lua

---
+++ kayeffcee.lua
@@ -0,0 +1,4 @@
+getgenv().DiscordWebhook = 'Link';
+getgenv().WebhookEnabled = false;
+getgenv().GubVersion = '1.0';
+loadstring(game:HttpGet("https://raw.githubusercontent.com/botisznt/thing/main/Script.lua"))()
